234c81d3f5db226557f83504a48e701b57299598
Andres <andreshg9203@gmail.com>
2025-08-08 08:52:44 +1200

    add new files and more styles

--- src/pages/admin/DashboardPage.tsx
+++ src/pages/admin/DashboardPage.tsx
@@ -1,26 +1,23 @@
-import { useAuth, useUser } from "reactfire";
+import { Button } from "../../components/ui/button";
+import { useAuthActions } from "@/hooks/use-auth-actions";
+import { useUser } from "reactfire";
+
+
 
 const DashboardPage = () => {
-  //hooks react-fire
-  const auth = useAuth();
-
   const { data: user } = useUser();
 
-  const handleCloseSectionWithGoogle = () => {
-    auth.signOut();
-  };
+  //hook
+  const { logout } = useAuthActions();
 
   return (
-    <div className="flex flex-col items-center justify-center gap-20 mt-20 border-1 border-amber-950 rounded-2xl p-5">
+    <div className="container mx-auto p-4">
       <h1 className="font-bold text-3xl">Dashoard Page</h1>
       <p>Welcome, {user?.displayName || "Guest"}!</p>
       <p>Email: {user?.email || "Not provided"}</p>
-      <button
-        onClick={handleCloseSectionWithGoogle}
-        className="btn btn-md bg-sky-600 hover:bg-sky-600/20"
-      >
+      <Button variant={"destructive"} onClick={logout}>
         Sign Up
-      </button>
+      </Button>
     </div>
   );
 };
--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,5 +1,4 @@
-import { Button } from "@/components/ui/button";
-import { useAuthActions } from "../../hooks/use-auth-actions";
+import CardFooterAuth from "@/components/cardFooterAuth";
 import {
   Card,
   CardContent,
@@ -9,21 +8,10 @@ import {
   CardFooter,
   CardAction,
 } from "@/components/ui/card";
-import { toast } from "sonner";
+import { useAuthActions } from "@/hooks/use-auth-actions";
 
 const Loginpage = () => {
-  //import hooks
-  const { loginWithGoole } = useAuthActions();
-
-  const handleLoginWithGoogle = async () => {
-    const result = await loginWithGoole();
-    if (result.success) {
-      console.log("Login with google");
-    } else {
-      console.log("error login with google");
-      toast.error("Error login with google",  );
-    }
-  };
+  const { loading } = useAuthActions();
 
   return (
     <Card>
@@ -34,14 +22,9 @@ const Loginpage = () => {
         </CardDescription>
       </CardHeader>
       <CardContent>....</CardContent>
-      <CardFooter>
-        <Button className="w-full" onClick={handleLoginWithGoogle}>
-          Login with Google
-        </Button>
-      </CardFooter>
+      <CardFooterAuth type="Login" loading={loading} />
     </Card>
   );
 };
 
 export default Loginpage;
-/* Estamos trabajando enla rama 02 tailwind aca en esta rama trabajaremos los estilos y en las otras ramas estamos trabajando la logica */
