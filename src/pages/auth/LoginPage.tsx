ac05ad2ece3641c87f7a5d8822e2814edd54ca65
Andres <andreshg9203@gmail.com>
2025-08-07 13:14:49 +1200

    add hooks

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,5 +1,11 @@
+import { useAuthActions } from "../../hooks/use-auth-actions"
 
 const Loginpage = () => {
+
+
+  //import hooks 
+  const {loginWithGoole} = useAuthActions()
+
   return (
     <div>Loginpage</div>
   )
--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -1,33 +1,8 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { useAuth } from "reactfire";
-
 const RegisterPage = () => {
-
-  //llamamos el auth que lo traer react-fire
-  const auth = useAuth()
-
-
-  // login with credentials google
-  const handleClickGoogle = async () => {
-
-    try {
-      //utilizamos una nueva instancia del metodo GoogleAuthprovider
-      const provider = new GoogleAuthProvider()
-      const result = await signInWithPopup(auth, provider)
-
-      console.log('User signed in successfully')
-    } catch (error) {
-      console.log("Error signig in with google: ", error)
-    }
-
-  };
-
   return (
     <div className="flex flex-col gap-20 items-center justify-center h-screen">
       <h1>Register</h1>
-      <button 
-      onClick={handleClickGoogle}
-      className="btn btn-lg bg-red-700/50 hover:bg-red-600/20">
+      <button className="btn btn-lg bg-red-700/50 hover:bg-red-600/20">
         Sign In Google
       </button>
     </div>
