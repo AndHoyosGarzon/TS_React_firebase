1191161a202168e8500bd5c53602c4fc56f3c041
Andres <andreshg9203@gmail.com>
2025-08-08 08:21:37 +1200

    add styles and library shadcn

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -2,28 +2,25 @@ import { Navigate, Outlet } from "react-router";
 import { useSigninCheck } from "reactfire";
 
 const AuthLayout = () => {
-  
-  
   //hook react-fire trae infromacion importante sobre el acceso de los usuarios a la app
-  const{status, data: signIngCheckResult, hassEmitted } =useSigninCheck()
-  
-  
-  
+  const { status, data: signIngCheckResult, hassEmitted } = useSigninCheck();
+
   //mostrar un loading
-  if (status === 'loading') {
-    return <div>Loading...</div>
-  }
-  
-  // redirigir si el usuario no esta autenticado
-  if(status === 'success' && signIngCheckResult.signedIn){
-    console.log(signIngCheckResult.signedIn)
-    return <Navigate to='/admin' replace/>
+  if (status === "loading") {
+    return <div>Loading...</div>;
   }
 
+  // redirigir si el usuario no esta autenticado
+  if (status === "success" && signIngCheckResult.signedIn) {
+    console.log(signIngCheckResult.signedIn);
+    return <Navigate to="/admin" replace />;
+  }
 
   return (
-    <div>
-      <Outlet />
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-50">
+      <div className="max-w-md w-full">
+        <Outlet />
+      </div>
     </div>
   );
 };
--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,14 +1,47 @@
-import { useAuthActions } from "../../hooks/use-auth-actions"
+import { Button } from "@/components/ui/button";
+import { useAuthActions } from "../../hooks/use-auth-actions";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+  CardAction,
+} from "@/components/ui/card";
+import { toast } from "sonner";
 
 const Loginpage = () => {
+  //import hooks
+  const { loginWithGoole } = useAuthActions();
 
-
-  //import hooks 
-  const {loginWithGoole} = useAuthActions()
+  const handleLoginWithGoogle = async () => {
+    const result = await loginWithGoole();
+    if (result.success) {
+      console.log("Login with google");
+    } else {
+      console.log("error login with google");
+      toast.error("Error login with google",  );
+    }
+  };
 
   return (
-    <div>Loginpage</div>
-  )
-}
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-center text-xl font-bold">Login</CardTitle>
+        <CardDescription>
+          Login to your account using email and password or with Google.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>....</CardContent>
+      <CardFooter>
+        <Button className="w-full" onClick={handleLoginWithGoogle}>
+          Login with Google
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
 
-export default Loginpage
+export default Loginpage;
+/* Estamos trabajando enla rama 02 tailwind aca en esta rama trabajaremos los estilos y en las otras ramas estamos trabajando la logica */
